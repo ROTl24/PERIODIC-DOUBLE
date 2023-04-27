a5b59a24420a3e994c588d481ff3f077dc3ad282
ROTl24 <119035617+ROTl24@users.noreply.github.com>
2023-04-27 08:45:45 +0800

    优化

--- test.cpp
+++ test.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target)
+{
+    int left = 0, right = nums.size() - 1;
+    while (left < right) {
+        int sum = nums[left] + nums[right];
+        if (sum == target) {
+            return { left, right };
+        }
+        else if (sum < target) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+    return vector<int>();
+}
+
 int main()
 {
-	vector<int>nums;//输入一个数组
-	int target;//求和
-	int size;//元素个数
-	int n;
-	cin >> size;
-	for (int i = 0; i < size; i++)
-	{
-		cin >> n;
-		nums.push_back(n);
-	}
-	cin >> target;
-	for (int i = 0; i < size; i++) {
-		for (int j = i; j < size; j++) {
-			if (nums[i] + nums[j] == target) {
-				cout << "[" << i << "," << j << "]" << endl;
-			}
-		}
-	}
-	return 0;
+    vector<int> nums;
+    int target, n;
+    int size;
+    cin >> size;
+    for (int i = 0; i < size; i++) {
+        cin >> n;
+        nums.push_back(n);
+    }
+    cin >> target;
+    vector<int> result = twoSum(nums, target);
+    if (result.empty()) {
+        cout << "没有" << endl;
+    }
+    else {
+        cout << "[" << result[0] << "," << result[1] << "]";
+    }
+    return 0;
 }
